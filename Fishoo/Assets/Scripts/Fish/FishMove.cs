6f430a3ba25a6b540eef53f2429ef7ac4021c6e8
Kyoichi <tanso0730@gmail.com>
2020-03-29 19:51:38 +0900

    Tile and Cinemachine update

--- Fishoo/Assets/Scripts/Fish/FishMove.cs
+++ Fishoo/Assets/Scripts/Fish/FishMove.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 namespace Fish
 {
-
     //釣りゲーム中の魚の動きを管理する
     public class FishMove : MonoBehaviour
     {
+        [SerializeField]
+        Fish.FishMoveData data;
+
         // Start is called before the first frame update
         void Start()
         {
