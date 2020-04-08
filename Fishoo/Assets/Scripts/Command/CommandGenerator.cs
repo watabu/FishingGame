692c65ecf68192a8cb72924d4ed6267f4492cca8
Kyoichi <tanso0730@gmail.com>
2020-04-08 20:56:42 +0900

    Command System Update

---
+++ Fishoo/Assets/Scripts/Command/CommandGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandGenerator : MonoBehaviour
+{
+    [System.Serializable]
+    public class CommandData
+    {
+        public KeyCode key;
+        public GameObject prefab;
+    }
+    [Header("Referances")]
+    [SerializeField] private Transform commandParent;
+    [Header("Properties")]
+    [SerializeField] private GameObject commandPrefab;
+
+    [SerializeField] private List<CommandData> commandsData=new List<CommandData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Generate()
+    {
+        var obj = Instantiate(commandPrefab, commandParent).GetComponent<CommandScript>();
+        List<KeyCode> com = new List<KeyCode>() {KeyCode.A,KeyCode.B,KeyCode.UpArrow,KeyCode.DownArrow };
+        obj.SetCommand(com);
+    }
+
+    public GameObject GetCommandPrefab(KeyCode key)
+    {
+        foreach (var i in commandsData)
+        {
+            if (i.key == key) return i.prefab;
+        }
+        return null;
+    }
+
+}
