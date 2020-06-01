98de8ac3d4ad68b1d9fcdc40e2c0fd6b36dd43f1
Kyoichi <tanso0730@gmail.com>
2020-06-01 19:06:52 +0900

    Pause UI update

--- Fishoo/Assets/Scripts/SceneFishbook/BookScritpUIMgr.cs
+++ Fishoo/Assets/Scripts/SceneFishbook/BookScritpUIMgr.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BookScritpUIMgr : MonoBehaviour
@@ -72,4 +73,11 @@ public class BookScritpUIMgr : MonoBehaviour
                 break;
         }
     }
+
+    public void BackToTitle()
+    {
+        SceneManager.LoadScene("StageSelect");
+
+    }
+
 }
