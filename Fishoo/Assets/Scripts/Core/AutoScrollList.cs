b68ff87932a4e310d3faa1457c89a220301b21c8
Kyoichi <tanso0730@gmail.com>
2020-06-08 20:53:53 +0900

    add fish

---
+++ Fishoo/Assets/Scripts/Core/AutoScrollList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+//https://qiita.com/toRisouP/items/3619ad3d2b7d785968f1
+
+public class AutoScrollList : MonoBehaviour
+{
+    [Header("Scroll")]
+    [SerializeField] private ScrollRect _scrollRect;
+    [SerializeField] private RectTransform _contentTransform;
+    [SerializeField] private RectTransform _viewportRectransform;
+    [Header("Debug")]
+    [SerializeField, ReadOnly] private GameObject currentGameObject;
+    [SerializeField, ReadOnly] private float currentTop;
+    [SerializeField, ReadOnly] private float currentBottom;
+    [SerializeField, ReadOnly] private float centerPosition;
+    [SerializeField, ReadOnly] private float topPosition;
+    [SerializeField, ReadOnly] private float bottomPosition;
+    [SerializeField, ReadOnly] private float viewportSize;
+    [SerializeField, ReadOnly] private float contentSize;
+    [SerializeField, ReadOnly] private float parentY;
+    /// <summary>
+    /// 自動スクロール
+    /// </summary>
+    public void Scroll()
+    {
+        currentGameObject = EventSystem.current.currentSelectedGameObject;
+        parentY = currentGameObject.transform.parent.position.y;
+        RectTransform current = (currentGameObject.transform) as RectTransform;
+        currentTop = current.offsetMin.y;
+        currentBottom = current.offsetMax.y;
+
+        //現在のスクロール範囲の数値を計算しやすい様に上下反転
+        var p = 1.0f - _scrollRect.verticalNormalizedPosition;
+
+        //描画範囲のサイズ
+        viewportSize = _viewportRectransform.rect.height;
+        //描画範囲のサイズの半分
+        var harlViewport = viewportSize * 0.5f;
+
+        contentSize = _contentTransform.sizeDelta.y;
+
+        //現在の描画範囲の中心座標
+        centerPosition = -(contentSize - viewportSize) * p - harlViewport;
+        //現在の描画範囲の上端座標
+        topPosition = centerPosition - harlViewport;
+        //現在の現在描画の下端座標
+        bottomPosition = centerPosition + harlViewport;
+
+        //選択した要素が上側にはみ出ている
+        if (currentTop < topPosition)
+        {
+            //選択要素が描画範囲に収まるようにスクロール
+            _scrollRect.verticalNormalizedPosition -= 0.01f;
+            return;
+        }
+
+        //選択した要素が下側にはみ出ている
+        if (currentBottom > bottomPosition)
+        {
+            _scrollRect.verticalNormalizedPosition += 0.01f;
+        }
+    }
+}
--- Fishoo/Assets/Scripts/SceneFishbook/BookScritpUIMgr.cs
+++ Fishoo/Assets/Scripts/SceneFishbook/BookScritpUIMgr.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -15,32 +14,34 @@ public class BookScritpUIMgr : MonoBehaviour
         description
     }
 
-
     [SerializeField] GameObject fishPrefab;
     [Header("References")]
     [SerializeField] GameObject bookList;
     [SerializeField] GameObject bookListContent;
     [SerializeField] GameObject bookDescription;
-    BookDescriptionUI descriptionUI;
     [SerializeField] Button SelectedButton;
     [SerializeField] FishInfoFolder FishFolder;
     [SerializeField] SaveData data;
     [SerializeField] Button backButton;
+    [Header("Debug")]
     [SerializeField] bool showNonCaughtFish = false;
+
+    AutoScrollList autoScroll;
+    BookDescriptionUI descriptionUI;
 
     State m_currentState = State.none;
     float m_changeTime = 0f;//切り替わってから何秒経ったか
 
-
     private void Awake()
     {
         descriptionUI = bookDescription.GetComponent<BookDescriptionUI>();
+        autoScroll = GetComponent<AutoScrollList>();
+        m_currentState = State.none;
     }
-    void Start()
+
+    void SetData(IEnumerable<FishInfo> list)
     {
-        FishFolder.InitiateList();
-        FishButtonUIScript lastObj = null;//最後に生成されたボタン(最初に生成されたボタンは右下に追いやられるようなので？要検討)
-        foreach (var d in FishFolder.AvailableFishes)
+        foreach (var d in list)
         {
             if (d == null) continue;
             var script = Instantiate(fishPrefab, bookListContent.transform).GetComponent<FishButtonUIScript>();
@@ -53,7 +54,6 @@ public class BookScritpUIMgr : MonoBehaviour
                     descriptionUI.Set(d.icon, d.FishName, d.description, data.fishes[d].count);
                     SelectedButton = script.GetButton;
                 });
-
             }
             else if (showNonCaughtFish)
             {
@@ -66,42 +66,19 @@ public class BookScritpUIMgr : MonoBehaviour
             }
             script.Interactable = showNonCaughtFish || data.isCaught(d);
             script.icon.sprite = d.icon;
-            lastObj = script;
         }
-
-        foreach (var d in FishFolder.AvailableGomi)
-        {
-            if (d == null) continue;
-            var script = Instantiate(fishPrefab, bookListContent.transform).GetComponent<FishButtonUIScript>();
-            //捕まえたなら詳しい情報を見れる
-            if (showNonCaughtFish || data.isCaught(d))
-            {
-                script.SetOnClicked(() =>
-                {
-                    Switch(State.description);
-                    descriptionUI.Set(d.icon, d.FishName, d.description, data.fishes[d].count);
-                    SelectedButton = script.GetButton;
-                });
-            }
-            else if (showNonCaughtFish)
-            {
-                script.SetOnClicked(() =>
-                {
-                    Switch(State.description);
-                    descriptionUI.Set(d.icon, d.FishName, d.description, 0);
-                    SelectedButton = script.GetButton;
-                });
-            }
-            script.Interactable = showNonCaughtFish || data.isCaught(d);
-            script.icon.sprite = d.icon;
-            lastObj = script;
-        }
-        //図鑑に戻ったときの初期選択
-        if (SelectedButton == null) SelectedButton = lastObj.GetButton;
-        Switch(State.list, true);
-        backButton.onClick.AddListener(() => { SceneManager.LoadScene("StageSelect"); });
     }
 
+    void Start()
+    {
+        backButton.onClick.AddListener(() => { SceneManager.LoadScene("StageSelect"); });
+        FishFolder.InitiateList();
+        SetData(FishFolder.AvailableFishes);
+        SetData(FishFolder.AvailableGomi);
+        //図鑑に戻ったときの初期選択
+        SelectedButton = bookListContent.transform.GetChild(0).GetComponent<Button>();
+        Switch(State.list, true);
+    }
 
     private void Update()
     {
@@ -113,14 +90,14 @@ public class BookScritpUIMgr : MonoBehaviour
             }
         }
         m_changeTime += Time.deltaTime;
-        currentGameObject = EventSystem.current.currentSelectedGameObject;
-        Scroll(currentGameObject);
+        autoScroll.Scroll();
     }
+
     public void Switch(State state, bool ignore = false)
     {
         if (!ignore && m_changeTime < 0.1f) return;//切り替わってから0.5秒経たないとシーン切り替えができないように
         if (m_currentState == state) return;
-        Debug.Log($"Switch {state}");
+        //Debug.Log($"Switch {state}");
         m_currentState = state;
         m_changeTime = 0f;
         switch (state)
@@ -129,6 +106,7 @@ public class BookScritpUIMgr : MonoBehaviour
                 bookList.SetActive(true);
                 bookDescription.SetActive(false);
                 backButton.interactable = true;
+                Debug.Log("Select", SelectedButton);
                 SelectedButton.Select();
                 break;
             case State.description:
@@ -141,61 +119,5 @@ public class BookScritpUIMgr : MonoBehaviour
         }
     }
 
-    [Header("Scroll")]
-    [SerializeField] private ScrollRect _scrollRect;
-    [SerializeField] private RectTransform _contentTransform;
-    [SerializeField] private RectTransform _viewportRectransform;
-    [Header("Debug")]
-    [SerializeField, ReadOnly] private GameObject currentGameObject;
-    [SerializeField, ReadOnly] private float currentTop;
-    [SerializeField, ReadOnly] private float currentBottom;
-    [SerializeField, ReadOnly] private float centerPosition;
-    [SerializeField, ReadOnly] private float topPosition;
-    [SerializeField, ReadOnly] private float bottomPosition;
-    [SerializeField, ReadOnly] private float viewportSize;
-    [SerializeField, ReadOnly] private float contentSize;
-    [SerializeField, ReadOnly] private float parentY;
-    /// <summary>
-    /// 自動スクロール
-    /// </summary>
-    void Scroll(GameObject current_)
-    {
-        parentY = current_.transform.parent.position.y;
-        RectTransform current = (current_.transform) as RectTransform;
-        currentTop = current.offsetMin.y;
-        currentBottom = current.offsetMax.y;
-
-        //現在のスクロール範囲の数値を計算しやすい様に上下反転
-        var p = 1.0f - _scrollRect.verticalNormalizedPosition;
-
-        //描画範囲のサイズ
-        viewportSize = _viewportRectransform.rect.height;
-        //描画範囲のサイズの半分
-        var harlViewport = viewportSize * 0.5f;
-
-        contentSize = _contentTransform.sizeDelta.y;
-
-        //現在の描画範囲の中心座標
-        centerPosition = -(contentSize - viewportSize) * p - harlViewport;
-        //現在の描画範囲の上端座標
-        topPosition = centerPosition - harlViewport;
-        //現在の現在描画の下端座標
-        bottomPosition = centerPosition + harlViewport;
-
-        //選択した要素が上側にはみ出ている
-        if (currentTop < topPosition)
-        {
-            //選択要素が描画範囲に収まるようにスクロール
-            _scrollRect.verticalNormalizedPosition -= 0.01f;
-            return;
-        }
-
-        //選択した要素が下側にはみ出ている
-        if (currentBottom > bottomPosition)
-        {
-            _scrollRect.verticalNormalizedPosition += 0.01f;
-        }
-    }
 }
 
-//https://qiita.com/toRisouP/items/3619ad3d2b7d785968f1
